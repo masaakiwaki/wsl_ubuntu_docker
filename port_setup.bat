019fb187add2cbfa33e1b34dd7297c2bca09251f
masaakiwaki <71519528+masaakiwaki@users.noreply.github.com>
2020-10-18 07:19:48 +0900

    Create port_setup.bat

---
+++ port_setup.bat
@@ -0,0 +1,11 @@
+### WSLのIP
+set wsl_ip=172.21.91.53
+
+### 転送ポート指定
+netsh interface portproxy add v4tov4 listenport=8888 listenaddr=* connectport=8888 connectaddress=%wsl_ip%
+
+### 転送ポート指定
+netsh interface portproxy show all
+
+### 転送ポート指定
+# netsh interface portproxy delete v4tov4 listenport=8888
